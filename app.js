be25a40251b09a60afd080e10c986ec15fa4236e
Aryan Kumar <lci2020004@iiitl.ac.in>
2023-12-08 14:28:09 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+import express from "express";
+import mongoose from "mongoose";
+import blogRouter from "./routes/blog-routes";
+import router from "./routes/user-routes";
+import cors from "cors";
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use("/api/user", router);
+app.use("/api/blog", blogRouter);
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => app.listen(process.env.PORT || 5000))
+  .then(() =>
+    console.log("Connected TO Database and Listening TO Localhost 5000")
+  )
+  .catch((err) => console.log(err));
