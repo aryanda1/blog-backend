b179e2882d304e312129d9d843d0db8d59f08e02
Aryan Kumar <lci2020004@iiitl.ac.in>
2024-01-25 16:08:58 +0530

    Updated project structure.

--- src/controllers/blog-controller.js
+++ src/controllers/blog-controller.js
@@ -19,7 +19,8 @@ export const addBlog = async (req, res, next) => {
   const { title, description, image, user } = req.body;
 
   let existingUser;
-  if(!mongoose.isObjectIdOrHexString(user)) return res.status(400).json({message:"invalid user Id"})
+  if (!mongoose.isObjectIdOrHexString(user))
+    return res.status(400).json({ message: "invalid user Id" });
   try {
     existingUser = await User.findById(user);
   } catch (err) {
